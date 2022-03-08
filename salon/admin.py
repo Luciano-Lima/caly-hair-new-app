9aafedca017a2091d1ec8fb05f5ace602e8b44f1
Luciano-Lima <ccll4ever1@gmail.com>
2022-03-08 15:47:12 +0000

    testing database config

--- salon/admin.py
+++ salon/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-# from .models import Cutting, Styling, Colour, Highlights, Toners, Treatment
-from .models import Cutting
+from .models import Cutting, Styling, Colour, Highlights, Toners, Treatment
+
 
 # # Register your models here.
-
 admin.site.register(Cutting)
-# admin.site.register(Styling)
-# admin.site.register(Colour)
-# admin.site.register(Highlights)
-# admin.site.register(Toners)
-# admin.site.register(Treatment)
+admin.site.register(Styling)
+admin.site.register(Colour)
+admin.site.register(Highlights)
+admin.site.register(Toners)
+admin.site.register(Treatment)
--- salon/models.py
+++ salon/models.py
@@ -14,53 +14,53 @@ class Cutting(models.Model):
         def __str__(self):
                 return self.verbose_name
 
-# class Styling(models.Model):
-#     name = models.CharField(max_length=200)
-#     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+class Styling(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
-#     class Meta:
-#         verbose_name = 'Up-Styling'
-#         verbose_name_plural = 'Up-Styling'
+    class Meta:
+        verbose_name = 'Up-Styling'
+        verbose_name_plural = 'Up-Styling'
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-# class Colour(models.Model):
-#     name = models.CharField(max_length=200)
-#     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+class Colour(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
-#     class Meta:
-#         verbose_name_plural = "Colour"
+    class Meta:
+        verbose_name_plural = "Colour"
 
-#     def __str__(self):
-#         return self.name   
+    def __str__(self):
+        return self.name   
 
-# class Highlights(models.Model):
-#     name = models.CharField(max_length=200)
-#     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+class Highlights(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
-#     class Meta:
-#         verbose_name_plural = 'highlight'
+    class Meta:
+        verbose_name_plural = 'highlight'
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-# class Toners(models.Model):
-#     name = models.CharField(max_length=200)
-#     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+class Toners(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
-#     class Meta:
-#         verbose_name_plural = 'Toner'
+    class Meta:
+        verbose_name_plural = 'Toner'
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
 
-# class Treatment(models.Model):
-#     name = models.CharField(max_length=200)
-#     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+class Treatment(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
-#     class Meta:
-#         verbose_name_plural = 'Treatment'
+    class Meta:
+        verbose_name_plural = 'Treatment'
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
