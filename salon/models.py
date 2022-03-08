6b2f163bcd8c68e1c2ec69df2cb8c5240ef18f30
Luciano-Lima <ccll4ever1@gmail.com>
2022-03-08 00:04:23 +0000

    making migrations

--- salon/models.py
+++ salon/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+
 # # Create your models here.
+
 class Cutting(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
