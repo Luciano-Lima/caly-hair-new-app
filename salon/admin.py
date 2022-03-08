b1630a7f20c6fa6217fce2cda979f591be6a84eb
Luciano-Lima <ccll4ever1@gmail.com>
2022-03-08 00:41:06 +0000

    Comenting all Models for testing

--- salon/admin.py
+++ salon/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Cutting, Styling, Colour, Highlights, Toners, Treatment
+# from .models import Cutting, Styling, Colour, Highlights, Toners, Treatment
 
-# Register your models here.
-admin.site.register(Cutting)
-admin.site.register(Styling)
-admin.site.register(Colour)
-admin.site.register(Highlights)
-admin.site.register(Toners)
-admin.site.register(Treatment)
+# # Register your models here.
+
+# admin.site.register(Cutting)
+# admin.site.register(Styling)
+# admin.site.register(Colour)
+# admin.site.register(Highlights)
+# admin.site.register(Toners)
+# admin.site.register(Treatment)
--- salon/views.py
+++ salon/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, redirect, reverse
-from .models import Cutting, Styling, Colour, Highlights, Toners, Treatment
+# from .models import Cutting, Styling, Colour, Highlights, Toners, Treatment
 
 
 # Create your views here.
+# def home(request):
+#     cutting = Cutting.objects.all().order_by('name')
+#     styling = Styling.objects.all().order_by('name')
+#     colour = Colour.objects.all().order_by('name')
+#     highlights = Highlights.objects.all().order_by('name')
+#     toners = Toners.objects.all().order_by('name')
+#     treatment = Treatment.objects.all().order_by('name')
+#     return render(request, 'index.html',
+#         {'cutting': cutting, 'styling': styling, 'colour': colour, 
+#         'highlights': highlights, 'toners': toners, 'treatment': treatment})
+
 def home(request):
-    cutting = Cutting.objects.all().order_by('name')
-    styling = Styling.objects.all().order_by('name')
-    colour = Colour.objects.all().order_by('name')
-    highlights = Highlights.objects.all().order_by('name')
-    toners = Toners.objects.all().order_by('name')
-    treatment = Treatment.objects.all().order_by('name')
-    return render(request, 'index.html',
-        {'cutting': cutting, 'styling': styling, 'colour': colour, 
-        'highlights': highlights, 'toners': toners, 'treatment': treatment})
+    return render(request, 'index.html')
+
